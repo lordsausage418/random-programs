fdaf7a25a79aa181cffd62464760b9cb23429ffc
Cirilaron <73666113+Cirilaron@users.noreply.github.com>
2020-11-26 21:01:29 +0300

    Add files via upload

---
+++ input and if elif else test.py
@@ -0,0 +1,11 @@
+print("is david the turtle pog")
+answer = input()
+if answer == "yes":
+    print(":) (press enter to exit)")
+    input()
+elif answer == "no":
+    print(":( (press enter to exit")
+    input()
+else:
+    print("uhh, yes or no, all lowercase, restart and try again, enter to exit")
+    input()
