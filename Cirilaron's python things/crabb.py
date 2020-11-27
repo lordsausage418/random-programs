805bc0df37c411b7597dd91632a315bbfd9fdd3e
Cirilaron <73666113+Cirilaron@users.noreply.github.com>
2020-11-28 00:35:47 +0300

    fix

--- Cirilaron's python things/crabb.py
+++
@@ -1,6 +0,0 @@
-while True:
-    a=input("Pray to crab god now")
-    print("ok, good I see u made ur prayer")
-    b=input("Now do you want to say another prayer?y/n")
-    if b=="n" or b=="N":
-        break
