60f2df65a65a3f6382c39b3c5ffd337a9c68cc9f
Cirilaron <ukirillavgostah8@gmail.com>
2020-12-08 14:20:17 +0000

    added Dark Mode Square to sigma and something else in project alpha

--- Cirilaron's python things/Project Sigma/project_sigma.py
+++ Cirilaron's python things/Project Sigma/project_sigma.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.configure(bg="black")
-root.geometry("500x500")
+root.geometry("650x570")
 root.title("Project Sigma")
 
 frame = tk.Frame(root, bg="black")
@@ -19,6 +19,35 @@ labelone.config(font=("Courier", 15))
 labeltwo = tk.Label(bottomframe, bg="black", fg="green", text="Press the button to open the program menu")
 labeltwo.config(font=("Courier", 15))
 labeltwo.pack(side="top")
+
+canvasboi = tk.Canvas(frame, bg="black", width=400, height=400, relief="flat")
+screenboi = turtle.TurtleScreen(canvasboi)
+screenboi.bgcolor("black")
+
+david = turtle.RawTurtle(screenboi)
+
+def darkmodesquare():
+  trianglerun.pack_forget()
+  prgrmsbutton.pack_forget()
+  prgrmsbutton.config(command=programsmenu)
+  prgrmsbutton.pack(side="bottom")
+  dmsbutton.pack_forget()
+  labelone.config(text="Here, have a square.")
+  labeltwo.config(text="drawing...")
+  david.color("white", "gray")
+  david.pensize(10)
+  canvasboi.pack(side="bottom")
+  david.fd(150)
+  david.left(90)
+  david.fd(150)
+  david.left(90)
+  david.fd(150)
+  david.left(90)
+  david.fd(150)
+  david.left(90)
+  labeltwo.config(text="like it? yeah you do.")
+
+dmsbutton = tk.Button(bottomframe, text="Dark Mode Square", command=darkmodesquare)
 
 def triangle():
     trianglestring = """    /\ 
@@ -45,12 +74,15 @@ def triangle():
 trianglerun = tk.Button(bottomframe, text="Triangle", command=triangle)
 
 def programsmenu():
-    labelone.config(text="Program List:\nTriangle: draws a triangle with ASCII\nPress Back to go to the main menu")
+    labelone.config(text="Program List:\nTriangle: draws a triangle with ASCII\nDark Mode Square: draws a white square using turtle\nPress Back to go to the main menu")
     labeltwo.config(text="Run a program:")
+    canvasboi.pack_forget()
     prgrmsbutton.pack_forget()
     prgrmsbutton.config(text="Back", command=mainmenu)
     prgrmsbutton.pack(side="left")
     trianglerun.pack(side="right")
+    dmsbutton.pack(side="bottom")
+    david.clear()
 
 def mainmenu():
     labelone.config(text="Hi, i'm a program made by Cirilaron.\n My name is Sigma.\n I will have most of his programs later.")
@@ -62,4 +94,4 @@ def mainmenu():
     
 prgrmsbutton = tk.Button(bottomframe, text="Programs", command=programsmenu)
 prgrmsbutton.pack(side="bottom")
-root.mainloop()
+root.mainloop() 
